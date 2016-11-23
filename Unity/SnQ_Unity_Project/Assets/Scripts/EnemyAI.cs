4201125ec5e1e4d1cfe007d55a1d8c668a96c2cb
FONS-ROCA12\Fons <twistfons@gmail.com>
2016-11-23 14:18:10 +0200

    Dag 1: Enemy gemaakt die een speler ziet en volgt. Moet er nog voor zorgen dat hij aanvalt en de y-positie van de grond laat afhangen.

---
+++ Unity/SnQ_Unity_Project/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour
+{
+     private GameObject goTarget;
+    public int id = 0;
+    public int iLives = 2;
+     protected float fDistance;
+    public float fSpeed = 3;
+    public float fPerceptionRange = 15;
+    
+	void Start ()
+    {
+        if (goTarget == null)
+            goTarget = GameObject.FindGameObjectWithTag("Player");
+
+        fDistance = CalculateDistance(goTarget);
+	}
+	
+	void Update ()
+    {
+        fDistance = CalculateDistance(goTarget);
+
+        if (fDistance < 3)
+        {
+            // attack
+        }
+        else if (fDistance < fPerceptionRange)
+        {
+            Vector3 direction = goTarget.transform.position - transform.position;
+            direction.Normalize();
+            transform.position += direction * fSpeed * Time.deltaTime;
+        }
+	}
+
+    float CalculateDistance(GameObject Target)
+    {
+        float x_dist = this.transform.position.x - goTarget.transform.position.x;
+        x_dist *= x_dist;
+        float y_dist = this.transform.position.y - goTarget.transform.position.y;
+        y_dist *= y_dist;
+        float z_dist = this.transform.position.z - goTarget.transform.position.z;
+        z_dist *= z_dist;
+
+        float return_value = Mathf.Sqrt(x_dist + y_dist + z_dist);
+        Debug.Log("Enemy ID " + id + " distance is " + return_value);
+        return return_value;
+    }
+}
