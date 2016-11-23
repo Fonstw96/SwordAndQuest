9bb82d9de476b457124e57a1a98cab5aca15b794
DESKTOP-KD8TKPH\jaspe <ha.jasper26@gmail.com>
2016-11-23 13:37:36 +0100

    player start

---
+++ Unity/SnQ_Unity_Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    private float x, y, z;
+    private float anglex, angley, anglez;
+
+
+	// Use this for initialization
+	void Start () {
+
+        this.transform.rotation = Quaternion.Euler(anglex, angley, anglez);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        angley = Input.GetAxis("Horizontal");
+        anglex = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            angley += .6f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            angley -= .6f;
+        }
+    }
+}
