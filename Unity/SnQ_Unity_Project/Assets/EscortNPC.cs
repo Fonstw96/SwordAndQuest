11eb6bd91a677d36553f80ab68ccb95e03931b10
MSI\Admin <delano-vd@hotmail.com>
2016-12-22 10:36:48 +0100

    commit

---
+++ Unity/SnQ_Unity_Project/Assets/EscortNPC.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscortNPC : MonoBehaviour {
+
+
+    void Start () {
+
+    }
+
+	void Update () {
+	    
+	}
+
+    public void Arrived()
+    {
+
+    }
+
+    public void Followpath()
+    {
+
+    }
+
+}
