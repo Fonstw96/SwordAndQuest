20f2665e5c87d41491eb8683f7565fae3a0491ec
DESKTOP-KD8TKPH\jaspe <ha.jasper26@gmail.com>
2017-01-25 12:08:59 +0100

    Revert "cya"
    This reverts commit e437815cf6b797ae32d7f37ed78dc8197974dbc1.

---
+++ Unity/SnQ_Unity_Project/Assets/Scripts/SwordSpawnController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordSpawnController : MonoBehaviour {
+
+    public GameObject Sword;
+    private int random;
+    private string SpawnLocation;
+
+    // Use this for initialization
+    void Start()
+    {
+        int Scene1Completed = PlayerPrefs.GetInt("Scene1Completed");
+        if (Scene1Completed == 0)
+        {
+            random = Random.Range(0, 5);
+            Instantiate(Sword);
+            random++;
+            //random = 1;
+            for (int i = 1; i < 6; i++)
+            {
+                SpawnLocation = "SwordSpawn" + i;
+                if (random == i)
+                {
+                    Sword.transform.position = GameObject.FindGameObjectWithTag(SpawnLocation).transform.position;
+                    Sword.transform.rotation = GameObject.FindGameObjectWithTag(SpawnLocation).transform.rotation;
+                }
+                Destroy(GameObject.FindGameObjectWithTag(SpawnLocation));
+            }
+            Debug.Log(random);
+        }
+        else
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                SpawnLocation = "SwordSpawn" + i;
+                Destroy(GameObject.FindGameObjectWithTag(SpawnLocation));
+            }
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
---
+++ Unity/SnQ_Unity_Project/Assets/Scripts/WeaponPickUp.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponPickUp : MonoBehaviour {
+
+    public GameObject myWeapon;
+    public GameObject WeaponOnGround;
+
+    private GameObject player;
+
+    public Player playerscript;
+
+	// Use this for initialization
+	void Start () {
+        myWeapon.SetActive(false);
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        playerscript = player.GetComponent<Player>();
+    }
+
+    void OnTriggerEnter(Collider _collider)
+    {
+        int Scene1Completed = PlayerPrefs.GetInt("Scene1Completed");
+        if (Scene1Completed == 0)
+        {
+
+            if (_collider.gameObject.tag == "Player")
+            {
+                myWeapon.SetActive(true);
+                WeaponOnGround.SetActive(false);
+
+                playerscript.sword = true;
+            }
+        }
+    }
+    
+}
